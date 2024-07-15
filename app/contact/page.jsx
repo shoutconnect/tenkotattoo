6320d7c76d97051fde51aafd1c5e6bb82c7c7bde
Mayranne <102763784+heydut@users.noreply.github.com>
2024-07-15 17:07:07 +0100

    added new contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,4 +1,5 @@
 import PageHeader from "@/components/UI/PageHeader";
+import Features from "@/components/gettattooed/Features";
 import CTA from "@/components/home/CTA";
 import { StudioImg } from "@/data/DataImages";
 import Image from "next/image";
@@ -27,6 +28,35 @@ export default function Contact() {
           draggable="false"
           className="-z-[1] absolute absolute-center w-full min-w-[1000px] opacity-50"
         />
+        <Features />
+      </div>
+      <div className="mt-20">
+        <div className="pb-10">
+          <div className="margin-x relative z-10">
+            <p className="text-center mb-10 pt-10">
+              <span className="font-bold">
+                Please complete the form in its entirety.
+              </span>{" "}
+              <br />
+              <span className="opacity-70">
+                {" "}
+                This ensures our artists can provide an accurate quote and
+                timeframe for your desired tattoo.
+              </span>
+            </p>
+            <iframe
+              src="
+https://www.cognitoforms.com/f/0RwzHuYrgkimJq8sYGUzwQ/4"
+              height="450"
+              width="100%"
+              className=""
+            ></iframe>
+            <script
+              src="
+https://www.cognitoforms.com/f/iframe.js"
+            ></script>
+          </div>
+        </div>{" "}
       </div>
       <CTA />
     </main>
