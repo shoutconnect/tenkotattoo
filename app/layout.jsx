e9396f4d64f76c4d550326cde1b8a2dfa55855c3
Mayranne <102763784+heydut@users.noreply.github.com>
2024-07-16 16:28:40 +0100

    replaced favicon

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,40 +2,16 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import PrelineScript from "@/components/PrelineScript";
-import Head from "next/head";
+
+export const metadata = {
+  title: "Tenko Tattoo",
+  description:
+    "Located in the heart of Preston/UK, our tattoo studio offers a diverse range of styles and services to bring your tattoo vision to life. Experience top-quality artistry and professional service in a welcoming environment.",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <title>Tenko Tattoo</title>
-        <meta
-          name="description"
-          content="Located in the heart of Preston/UK, our tattoo studio offers a diverse range of styles and services to bring your tattoo vision to life. Experience top-quality artistry and professional service in a welcoming environment."
-        />
-
-        {/* Open Graph Meta Tags */}
-        <meta property="og:title" content="Tenko Tattoo" />
-        <meta
-          property="og:description"
-          content="Located in the heart of Preston/UK, our tattoo studio offers a diverse range of styles and services to bring your tattoo vision to life. Experience top-quality artistry and professional service in a welcoming environment."
-        />
-        <meta property="og:image" content="/images/logos/Tenko SVG Black.svg" />
-        <meta property="og:url" content="https://www.tenkotattoo.com/" />
-        <meta property="og:type" content="website" />
-
-        {/* Twitter Card Meta Tags (Optional) */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Tenko Tattoo" />
-        <meta
-          name="twitter:description"
-          content="Located in the heart of Preston/UK, our tattoo studio offers a diverse range of styles and services to bring your tattoo vision to life. Experience top-quality artistry and professional service in a welcoming environment."
-        />
-        <meta
-          name="twitter:image"
-          content="/images/logos/Tenko SVG Black.svg"
-        />
-      </Head>
       <body>
         <PrelineScript />
         <div className="fixed mt-36 mx-auto right-0 left-0 -z-20 inset-0 h-[100%] w-[95%] bg-neutral-50 bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]"></div>
