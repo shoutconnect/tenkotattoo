8d038dcba21ff5760256ff1830945c1935cbb22e
Mayranne <102763784+heydut@users.noreply.github.com>
2024-07-19 12:50:40 +0100

    add takey description

--- data/DataArtist.jsx
+++ data/DataArtist.jsx
@@ -64,9 +64,22 @@ export const DataArtist = [
     name: "Takey",
     url: "/artists/takey",
     logo: "",
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-
+    description: (
+      <>
+        Introducing Takey, who recently moved to the United Kingdom in 2023.
+        With over 11 years in the tattoo industry, Takey previously owned his
+        own studio in Hong Kong and couldn’t wait to pick up his machine again.
+        He joined Tenko Tattoo in May 2024.
+        <br />
+        <br />
+        Not wanting to restrict his creativity to any one style, Takey has
+        excelled in calligraphy, fine line, and oriental artwork, among others.
+        <br />
+        <br />
+        Outside of the studio, Takey enjoys photography and occasionally
+        indulges in woodworking projects.
+      </>
+    ),
     facebook: "/",
     facebookURL: "/",
     instagram: "@takeytattoo.uk",
