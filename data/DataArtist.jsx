2bf9317a1082ba455e5665c8c0ce50f625c47db7
Mayranne <102763784+heydut@users.noreply.github.com>
2024-07-19 12:47:16 +0100

    added doris description

--- data/DataArtist.jsx
+++ data/DataArtist.jsx
@@ -3,8 +3,22 @@ export const DataArtist = [
     name: "Doris",
     url: "/artists/doris",
     logo: "/images/logos/Dorisu.svg",
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    description: (
+      <>
+        Meet Doris, with over 7 years of experience tattooing, Doris has defined
+        her unique style as an artist. Specialising in pop-culture, anime, neo
+        traditional and the occasional blackwork.
+        <br />
+        <br />
+        When not tattooing, Doris loves to get creative with making jewellery,
+        and can't help but rewatch the same classic shows.
+        <br />
+        <br />
+        Heavily influenced by Japanese art, her vision when creating Tenko
+        Tattoo was inspired by everything she really loves about the folklore
+        and creativity of Japanese culture.
+      </>
+    ),
     facebook: "@dorisu_tattoo",
     facebookURL: "https://www.facebook.com/dorisu_tattoo",
     instagram: "@dorisu_tattoo",
