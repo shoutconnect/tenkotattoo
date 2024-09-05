86b1c97d032e6ccbb1dc0dba09626d50d345c7d1
Mayranne <102763784+heydut@users.noreply.github.com>
2024-09-05 12:38:20 +0100

    updated robots

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/get-tattooed/thank-you",
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "noindex, nofollow",
+          },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
